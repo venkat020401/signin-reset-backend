99f5ec5fa5b70bb4168fe8db2aa904afc84f4f46
venkat020401 <venkat020401@gmail.com>
2023-02-06 17:12:38 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,191 @@
+const express = require("express");
+const app = express();
+const mongodb = require("mongodb");
+const dotenv = require("dotenv").config();
+const mongoclient = mongodb.MongoClient;
+const URL = process.env.DB;
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const SECURT = process.env.jwt_secret;
+const rn = require('random-number');
+
+const options = {
+  min: 1000,
+  max: 9999,
+  integer: true
+}
+
+//midleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// register
+app.post("/register", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("registrations");
+    const collection = db.collection("adminReg");
+
+    const salt1 = await bcrypt.genSalt(10);
+    const hash1 = await bcrypt.hash(req.body.password, salt1);
+    req.body.password = hash1;
+
+    const salt2 = await bcrypt.genSalt(10);
+    const hash2 = await bcrypt.hash(req.body.confirm_password, salt2);
+    req.body.confirm_password = hash2;
+
+    const users = await collection.insertOne(req.body);
+    await connection.close();
+
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+// login
+app.post("/login", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("registrations");
+    const collection = db.collection("adminReg");
+    const user = await collection.findOne({ email: req.body.email });
+    console.log(user);
+    if (user == null) {
+      res.status(200).json({ message: "User not found", token });
+    } else if(user){
+      const compare = await bcrypt.compare(req.body.password, user.password);
+      if (compare) {
+        const token = jwt.sign({ id: user._id }, SECURT);
+        res.json({ message: "Login success" });
+      } else {
+        res.status(200).json({ message: "email/password wrong", token });
+      }
+    }
+    
+    else {
+      res.json({ message: "Email / Password Is Wrong" });
+    }
+
+    await connection.close();
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+
+// email config
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+// Forgot Password mail sent
+app.post("/sendpasswordlink", async (req, res) => {
+
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("registrations");
+    const collection = db.collection("adminReg");
+    const userfind = await collection.findOne({ email: req.body.email });
+    if (userfind) {
+      let randomnum = rn(options);
+      const setrandomnum = await collection.findOneAndUpdate({ email: req.body.email }, {
+        $set: {
+          rnum: randomnum
+        }
+      });
+
+      if (setrandomnum) {
+        console.log(setrandomnum);
+        const mailOptions = {
+          from: process.env.EMAIL,
+          to: req.body.email,
+          subject: "Sending Email For password Reset",
+          html: `Please <a href='http://localhost:3000/verify-user/${setrandomnum.value._id}/${randomnum}'> Click here</a> to rest your password`
+        }
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            res.status(401).json({ status: 401, message: "email not send" });
+          } else {
+            res.status(201).json({ status: 201, message: "Email sent Succsfully" });
+          }
+        });
+      }
+    }
+    else {
+      res.status(401).json({ status: 401, message: "user not send" });
+    }
+
+
+  } catch (error) {
+    console.log("err", error);
+  }
+
+});
+
+//verify user to forgot password
+app.post("/verify-user/:id/:randomnum", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("registrations");
+    const collection = db.collection("adminReg");
+    const userfind = await collection.findOne({ _id: mongodb.ObjectId(req.params.id) });
+
+    if (userfind.rnum == req.params.randomnum) {
+      res.status(200).json({ message: "user verified" });
+    }
+    else {
+      res.status(400).json({ message: "Invalid url" })
+    }
+    await connection.close();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//update new password
+app.put("/password-update/:id", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("registrations");
+    const collection = db.collection("adminReg");
+
+    const salt1 = await bcrypt.genSalt(10);
+    const hash1 = await bcrypt.hash(req.body.password, salt1);
+    req.body.password = hash1;
+
+    const salt2 = await bcrypt.genSalt(10);
+    const hash2 = await bcrypt.hash(req.body.confirm_password, salt2);
+    req.body.confirm_password = hash2;
+
+    const users = await collection.findOneAndUpdate({ _id: mongodb.ObjectId(req.params.id) }, {
+      $set: {
+        password: hash1,
+        confirm_password: hash2
+      }
+    });
+
+    await connection.close();
+    res.json({ message: "Password updated successfully" })
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+// set port.
+app.listen(8000);
+
---
+++ test.js
@@ -0,0 +1,14 @@
+{
+    "_id": {
+      "$oid": "63dfbce4a1d3485c60ed0df3"
+    },
+    "First_name": "venki",
+    "Last_name": "venki",
+    "email": "venkat020401@gmail.com",
+    "password": "$2a$10$GMTCg6A3CrQHgv9qHONOteUNF.dzK5Qgk3orAvB/LP1/P.IWXEgDm",
+    "confirm_password": "$2a$10$o.t3BNo4OruRQOVBGHar9.FQnYSmaTNgFcGW1GfnxP4INtS/z6A56",
+    "verifytoken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGZiY2U0YTFkMzQ4NWM2MGVkMGRmMyIsImlhdCI6MTY3NTYwNzQ3NH0.b4HXUQCX6jAcnvE2quNVsDDvnV6vjjRfUh-GpxxfRxM"
+  }
+
+
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZGZiY2U0YTFkMzQ4NWM2MGVkMGRmMyIsImlhdCI6MTY3NTYwNzMyNX0.myhFBN4BwGhWocpWOEiUw7eAcYOFtLUaEl2tryGgbSs
