f65499501e8a982ea3fff9019373d1ac30e62e2c
venkat020401 <venkat020401@gmail.com>
2023-02-07 00:56:59 +0530

    first

--- index.js
+++ index.js
@@ -9,28 +9,29 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const SECURT = process.env.jwt_secret;
-const rn = require('random-number');
+const rn = require("random-number");
+const client_url = "http://localhost:3000"
 
 const options = {
   min: 1000,
   max: 9999,
-  integer: true
-}
+  integer: true,
+};
 
-//midleware
+//Middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://auth2512.netlify.app",
+    origin: client_url,
   })
 );
 
-// register
+// Register
 app.post("/register", async (req, res) => {
   try {
     const connection = await mongoclient.connect(URL);
     const db = connection.db("registrations");
-    const collection = db.collection("adminReg");
+    const collection = db.collection("datas");
 
     const salt1 = await bcrypt.genSalt(10);
     const hash1 = await bcrypt.hash(req.body.password, salt1);
@@ -50,17 +51,17 @@ app.post("/register", async (req, res) => {
   }
 });
 
-// login
+// Login
 app.post("/login", async (req, res) => {
   try {
     const connection = await mongoclient.connect(URL);
     const db = connection.db("registrations");
-    const collection = db.collection("adminReg");
+    const collection = db.collection("datas");
     const user = await collection.findOne({ email: req.body.email });
     console.log(user);
     if (user == null) {
       res.status(200).json({ message: "User not found", token });
-    } else if(user){
+    } else if (user) {
       const compare = await bcrypt.compare(req.body.password, user.password);
       if (compare) {
         const token = jwt.sign({ id: user._id }, SECURT);
@@ -68,45 +69,43 @@ app.post("/login", async (req, res) => {
       } else {
         res.status(200).json({ message: "email/password wrong", token });
       }
-    }
-    
-    else {
+    } else {
       res.json({ message: "Email / Password Is Wrong" });
     }
 
     await connection.close();
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     res.status(500).json({ message: "something went wrong" });
   }
 });
 
-
-// email config
+// Email config
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.PASSWORD
-  }
+    pass: process.env.PASSWORD,
+  },
 });
 
 // Forgot Password mail sent
 app.post("/sendpasswordlink", async (req, res) => {
-
   try {
     const connection = await mongoclient.connect(URL);
     const db = connection.db("registrations");
-    const collection = db.collection("adminReg");
+    const collection = db.collection("datas");
     const userfind = await collection.findOne({ email: req.body.email });
     if (userfind) {
       let randomnum = rn(options);
-      const setrandomnum = await collection.findOneAndUpdate({ email: req.body.email }, {
-        $set: {
-          rnum: randomnum
+      const setrandomnum = await collection.findOneAndUpdate(
+        { email: req.body.email },
+        {
+          $set: {
+            rnum: randomnum,
+          },
         }
-      });
+      );
 
       if (setrandomnum) {
         console.log(setrandomnum);
@@ -114,8 +113,8 @@ app.post("/sendpasswordlink", async (req, res) => {
           from: process.env.EMAIL,
           to: req.body.email,
           subject: "Sending Email For password Reset",
-          html: `Please <a href='https://auth2512.netlify.app/verify-user/${setrandomnum.value._id}/${randomnum}'> Click here</a> to rest your password`
-        }
+          html: `<b>Please <a href='${client_url}/verify-user/${setrandomnum.value._id}/${randomnum}'> Click here</a> to reset your password</b>`,
+        };
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
             res.status(401).json({ status: 401, message: "email not send" });
@@ -124,16 +123,12 @@ app.post("/sendpasswordlink", async (req, res) => {
           }
         });
       }
-    }
-    else {
+    } else {
       res.status(401).json({ status: 401, message: "user not send" });
     }
-
-
   } catch (error) {
     console.log("err", error);
   }
-
 });
 
 //verify user to forgot password
@@ -141,14 +136,15 @@ app.post("/verify-user/:id/:randomnum", async (req, res) => {
   try {
     const connection = await mongoclient.connect(URL);
     const db = connection.db("registrations");
-    const collection = db.collection("adminReg");
-    const userfind = await collection.findOne({ _id: mongodb.ObjectId(req.params.id) });
+    const collection = db.collection("datas");
+    const userfind = await collection.findOne({
+      _id: mongodb.ObjectId(req.params.id),
+    });
 
     if (userfind.rnum == req.params.randomnum) {
       res.status(200).json({ message: "user verified" });
-    }
-    else {
-      res.status(400).json({ message: "Invalid url" })
+    } else {
+      res.status(400).json({ message: "Invalid url" });
     }
     await connection.close();
   } catch (error) {
@@ -156,12 +152,12 @@ app.post("/verify-user/:id/:randomnum", async (req, res) => {
   }
 });
 
-//update new password
+//Update new password
 app.put("/password-update/:id", async (req, res) => {
   try {
     const connection = await mongoclient.connect(URL);
     const db = connection.db("registrations");
-    const collection = db.collection("adminReg");
+    const collection = db.collection("datas");
 
     const salt1 = await bcrypt.genSalt(10);
     const hash1 = await bcrypt.hash(req.body.password, salt1);
@@ -171,21 +167,21 @@ app.put("/password-update/:id", async (req, res) => {
     const hash2 = await bcrypt.hash(req.body.confirm_password, salt2);
     req.body.confirm_password = hash2;
 
-    const users = await collection.findOneAndUpdate({ _id: mongodb.ObjectId(req.params.id) }, {
-      $set: {
-        password: hash1,
-        confirm_password: hash2
+    const users = await collection.findOneAndUpdate(
+      { _id: mongodb.ObjectId(req.params.id) },
+      {
+        $set: {
+          password: hash1,
+          confirm_password: hash2,
+        },
       }
-    });
-
+    );
     await connection.close();
-    res.json({ message: "Password updated successfully" })
+    res.json({ message: "Password updated successfully" });
   } catch (error) {
     console.log(error);
   }
 });
 
-
-// set port.
+// Set port.
 app.listen(8000);
-
